ba55db86966382bebe223398dddada702b5de0ae
Yevhen Biliavskyi <kapai1890@gmail.com>
2018-04-20 01:09:17 +0300

    Create todebug-mu.php

---
+++ todebug-mu.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Plugin Name: Todebug
+ * Plugin URI: https://github.com/kapai1890/todebug
+ * Description: Debug logger with over 0 million downloads.
+ * Version: 18.16.1
+ * Author: kapai1890
+ * Author URI: https://github.com/kapai1890
+ * License: MIT
+ * Text Domain: todebug
+ * Domain Path: /languages
+ */
+
+/*
+ * Current file is a proxy PHP loader for must-use plugins (a.k.a. mu-plugins).
+ * Copy or move this file from todebug/ to WPMU_PLUGIN_DIR (by default it's
+ * folder wp-content/mu-plugins/).
+ */
+
+require_once 'todebug/todebug.php';
