e921bf74c876ed447f4c2cbe7cca8a7cd1c73c62
Yevhen Biliavskyi <kapai1890@gmail.com>
2018-04-20 01:07:18 +0300

    Create wordpress-admin.js

---
+++ assets/wordpress-admin.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function () {
+    "use strict";
+
+    var $logs = jQuery('#todebug-last-logs');
+
+    // Show/hide logs
+    jQuery('#wp-admin-bar-todebug a').on('click', function (event) {
+        event.preventDefault();
+        $logs.toggle();
+        jQuery(this).blur();
+    });
+
+    // Close logs by click
+    $logs.on('click', function (event) {
+        $logs.toggle();
+    });
+
+    // Disable event propagation on .inner-wrapper to allow to highlight and
+    // copy the text
+    $logs.children('.inner-wrapper').on('click', function (event) {
+        event.stopPropagation();
+    });
+});
