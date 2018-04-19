0788574c9dadcc742b4da76759042109b1278b4d
Yevhen Biliavskyi <kapai1890@gmail.com>
2018-04-20 01:09:00 +0300

    Create todebug.php

---
+++ todebug.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Plugin Name: Todebug
+ * Plugin URI: https://github.com/kapai1890/todebug
+ * Description: Debug logger with over 0 million downloads.
+ * Version: 18.16.1
+ * Author: kapai1890
+ * Author URI: https://github.com/kapai1890
+ * License: MIT
+ * Text Domain: todebug
+ * Domain Path: /languages
+ */
+
+if (!defined('ABSPATH')) {
+    exit('Use standalone version instead of WordPress plugin.');
+}
+
+define('TODEBUG_PLUGIN_FILE', __FILE__);
+
+require_once 'main.php';
